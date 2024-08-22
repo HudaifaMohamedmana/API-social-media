62f6f9789490768b558aef74c4b62e4ad0a8f03b
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-22 09:35:02 -0400

    buttons

--- app.mjs
+++ app.mjs
@@ -1,19 +1,8 @@
-import{postsFrame} from './post.mjs' ;
-
+import { food } from './food.mjs';
 document.querySelector('.food').addEventListener('click',()=>{
-    let images;
-    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php').then(Response =>{
-        images = Response.data
-        let imgurl = images.categories
-        console.log(images.categories);
-    //categories
-        for (let i = 0; i < imgurl.length; i++) {
-            console.log(`this is url ${i}`,imgurl[i]);
-            postsFrame(imgurl[i].strCategoryThumb);
-        }
-    })
+    
+    food()
+});
+document.querySelector('.clear').addEventListener('click',()=>{
+    window.location.reload()
 })
-
-
-
-
---
+++ food.mjs
@@ -0,0 +1,17 @@
+import{postsFrame} from './post.mjs' ;
+
+
+function food() {
+   let images;
+axios.get('https://www.themealdb.com/api/json/v1/1/categories.php').then(Response =>{
+    images = Response.data
+    let imgurl = images.categories
+    console.log(images.categories);
+//categories
+    for (let i = 0; i < imgurl.length; i++) {
+        console.log(`this is url ${i}`,imgurl[i]);
+        postsFrame(imgurl[i].strCategoryThumb);
+    }
+}) 
+}
+export{food};
