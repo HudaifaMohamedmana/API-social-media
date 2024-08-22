ee1a91bb9c36c4740c4c609bff23c7f55456e938
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-22 05:26:43 -0400

    first post

--- post.mjs
+++ post.mjs
@@ -1,24 +1,16 @@
-// import* as postModule from './post.mjs' ;
-// postModule('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWi2b0msfkDvV2_M6i6ryYu-FB4vKIjME2YA&s');
-
 const posts = document.querySelector('.MainPage');
  
 function postsFrame(src) {
     let newDiv = document.createElement('div')
     newDiv.setAttribute('class','postFram');
-
     let newImge = document.createElement('img')
     newImge.setAttribute('src',src);
-
     let reactionButtons = document.createElement('div')
-    reactionButtons.setAttribute('class','reaction');
-
+    reactionButtons.setAttribute('id','reaction');
     let r1 =  document.createElement('img')
     r1.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Love_or_Heart_transparent.png/1124px-Love_or_Heart_transparent.png');
-    
     let r2 =  document.createElement('img')
     r2.setAttribute('src',"https://static.vecteezy.com/system/resources/thumbnails/018/741/992/small/white-origami-paper-plane-deliver-message-symbol-interface-theme-3d-icon-render-illustration-isolated-png.png");
-    
     let r3 =  document.createElement('img')
     r3.setAttribute('src','https://icons.iconarchive.com/icons/custom-icon-design/flatastic-1/512/comment-icon.png');
 
@@ -29,11 +21,11 @@ function postsFrame(src) {
     reactionButtons.appendChild(r2);
     reactionButtons.appendChild(r3); 
 
-    
+    newDiv.appendChild(reactionButtons);
 
     newDiv.appendChild(newImge); 
-    newDiv.appendChild(reactionButtons);
+    
     posts.appendChild(newDiv);
 }
 
-postsFrame('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWi2b0msfkDvV2_M6i6ryYu-FB4vKIjME2YA&s')
+export{postsFrame};
