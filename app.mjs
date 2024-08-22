7a95a7fa03f84d37f918773034a1f6788379496d
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-22 09:07:28 -0400

    food

--- app.mjs
+++ app.mjs
@@ -1,9 +1,19 @@
 import{postsFrame} from './post.mjs' ;
-postsFrame('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWi2b0msfkDvV2_M6i6ryYu-FB4vKIjME2YA&s');
 
-postsFrame('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4dPkT7psW_yfhvCJBzUbg6VA_NBHp11obAw&s');
-postsFrame('https://media1.tenor.com/m/0oH_oZ43RxEAAAAd/luffy-gear-5.gif');
-postsFrame('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsoUNX2KDgKSaXD62GOjLQxEtpJ7NhyQ716Q&s');
-postsFrame('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShYs_23lhVgJYvyP-Xw4vE7bBcqPpESq_kEQ&s');
+document.querySelector('.food').addEventListener('click',()=>{
+    let images;
+    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php').then(Response =>{
+        images = Response.data
+        let imgurl = images.categories
+        console.log(images.categories);
+    //categories
+        for (let i = 0; i < imgurl.length; i++) {
+            console.log(`this is url ${i}`,imgurl[i]);
+            postsFrame(imgurl[i].strCategoryThumb);
+        }
+    })
+})
+
+
 
 
