ccf75f90ad3149875e0f5300e0ed5775e7e033a2
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-26 09:32:43 -0400

    async

--- food.mjs
+++ food.mjs
@@ -1,20 +1,20 @@
 import{postsFrame} from './post.mjs' ;
 
 let x;
-function food() {
-    let images;
-    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php').then(Response =>{
-    images = Response.data
-    let imgurl = images.categories
-    console.log(images.categories);
+async function food() {
+    const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+    const images = response.data;
+    const imgurl = images.categories;
+    console.log(imgurl);
+
 //categories
     for (let i = 0; i < imgurl.length; i++) {
         // console.log(`this is url ${i}`,imgurl[i]);
         x = i+1000
         postsFrame(imgurl[i].strCategoryThumb, x);
     }
-}) 
+
 }
+ 
 
-
-export{food};
+export { food };
